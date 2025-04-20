c1cb7f26d4ee38e466958642c3eb8f99811fa39b
alencarjunior-code <montival.alencar@gmail.com>
2025-04-19 21:01:56 -0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,21 @@
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open("grapiuna-cache").then(cache => {
+      return cache.addAll([
+        "./",
+        "./index.html",
+        "./styles.css",
+        "./script.js",
+        "./fundo3.jpg",
+        "./logo-extintores.png",
+        "./manifest.json"
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(response => response || fetch(e.request))
+  );
+});
